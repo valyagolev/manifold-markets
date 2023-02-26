6c5650e6705f18d107d895cb7c5481983c18ce43
Valentin Golev <v.golev@gmail.com>
2023-02-27 00:36:56 +0100

    all methods

--- src/client.rs
+++ src/client.rs
@@ -210,139 +210,6 @@ impl ManifoldClient {
             .await?)
     }
 
-    /*
-                    GET /v0/users
-                    Lists all users, ordered by creation date descending.
-
-                    Parameters:
-
-                    limit: Optional. How many users to return. The maximum and the default are 1000.
-                    before: Optional. The ID of the user before which the list will start. For example, if you ask for the most recent 10 users, and then perform a second query for 10 more users with before=[the id of the 10th user], you will get users 11 through 20.
-                    Requires no authorization.
-
-                    POST /v0/bet
-                    Places a new bet on behalf of the authorized user.
-
-                    Parameters:
-
-                    amount: Required. The amount to bet, in mana, before fees.
-                    contractId: Required. The ID of the contract (market) to bet on.
-                    outcome: Required. The outcome to bet on. For binary markets, this is YES or NO. For free response markets, this is the ID of the free response answer. For numeric markets, this is a string representing the target bucket, and an additional value parameter is required which is a number representing the target value. (Bet on numeric markets at your own peril.)
-                    limitProb: Optional. A number between 0.001 and 0.999 inclusive representing the limit probability for your bet (i.e. 0.1% to 99.9% — multiply by 100 for the probability percentage). The bet will execute immediately in the direction of outcome, but not beyond this specified limit. If not all the bet is filled, the bet will remain as an open offer that can later be matched against an opposite direction bet.
-                    For example, if the current market probability is 50%:
-                    A M$10 bet on YES with limitProb=0.4 would not be filled until the market probability moves down to 40% and someone bets M$15 of NO to match your bet odds.
-                    A M$100 bet on YES with limitProb=0.6 would fill partially or completely depending on current unfilled limit bets and the AMM's liquidity. Any remaining portion of the bet not filled would remain to be matched against in the future.
-                    An unfilled limit order bet can be cancelled using the cancel API.
-
-
-                    POST /v0/bet/cancel/[id]
-                    Cancel the limit order of a bet with the specified id. If the bet was unfilled, it will be cancelled so that no other bets will match with it. This action is irreversible.
-
-                    POST /v0/market
-                    Creates a new market on behalf of the authorized user.
-
-                    Note: this costs mana. If you have insufficient mana, this call will return an error. The cost to create each type of market is:
-
-                    Market Type	Creation Cost
-                    BINARY	M$50
-                    PSEUDO_NUMERIC	M$250
-                    FREE_RESPONSE	M$100
-                    MULTIPLE_CHOICE	M$100
-                    Parameters:
-
-                    outcomeType: Required. One of BINARY, FREE_RESPONSE, MULTIPLE_CHOICE, or PSEUDO_NUMERIC.
-                    question: Required. The headline question for the market.
-                    description: Optional. A long description describing the rules for the market.
-                    Note: string descriptions do not turn into links, mentions, formatted text. You may instead use descriptionMarkdown or descriptionHtml for rich text formatting.
-                    closeTime: Optional. The time at which the market will close, represented as milliseconds since the epoch. Defaults to 7 days from now.
-                    visibility: Optional. One of public (default) or unlisted. Controls whether the market can be shown on homepage and in search results.
-                    groupId: Optional. A group to create this market under.
-                    For binary markets, you must also provide:
-
-                    initialProb: An initial probability for the market, between 1 and 99.
-                    For numeric markets, you must also provide:
-
-                    min: The minimum value that the market may resolve to.
-                    max: The maximum value that the market may resolve to.
-                    isLogScale: If true, your numeric market will increase exponentially from min to max.
-                    initialValue: An initial value for the market, between min and max, exclusive.
-                    For multiple choice markets, you must also provide:
-
-                    answers: An array of strings, each of which will be a valid answer for the market.
-
-
-                POST /v0/market/[marketId]/add-liquidity
-                Adds a specified amount of liquidity into the market.
-
-                amount: Required. The amount of liquidity to add, in M$.
-                POST /v0/market/[marketId]/close
-                Closes a market on behalf of the authorized user.
-
-                closeTime: Optional. Milliseconds since the epoch to close the market at. If not provided, the market will be closed immediately. Cannot provide close time in the past.
-                POST /v0/market/[marketId]/resolve
-                Resolves a market on behalf of the authorized user.
-
-                Parameters:
-
-                For binary markets:
-
-                outcome: Required. One of YES, NO, MKT, or CANCEL.
-                probabilityInt: Optional. The probability to use for MKT resolution.
-                For free response or multiple choice markets:
-
-                outcome: Required. One of MKT, CANCEL, or a number indicating the answer index.
-                resolutions: An array of { answer, pct } objects to use as the weights for resolving in favor of multiple free response options. Can only be set with MKT outcome. Note that the total weights must add to 100.
-                For numeric markets:
-
-                outcome: Required. One of CANCEL, or a number indicating the selected numeric bucket ID.
-                value: The value that the market resolves to.
-                probabilityInt: Required if value is present. Should be equal to
-                If log scale: log10(value - min + 1) / log10(max - min + 1)
-                Otherwise: (value - min) / (max - min)
-
-            POST /v0/market/[marketId]/sell
-            Sells some quantity of shares in a binary market on behalf of the authorized user.
-
-            Parameters:
-
-            outcome: Optional. One of YES, or NO. If you leave it off, and you only own one kind of shares, you will sell that kind of shares.
-            shares: Optional. The amount of shares to sell of the outcome given above. If not provided, all the shares you own will be sold.
-
-        POST /v0/comment
-        Creates a comment in the specified market. Only supports top-level comments for now.
-
-        Parameters:
-
-        contractId: Required. The ID of the market to comment on.
-        content: The comment to post, formatted as TipTap json, OR
-        html: The comment to post, formatted as an HTML string, OR
-        markdown: The comment to post, formatted as a markdown string.
-
-
-        GET /v0/comments
-        Gets a list of comments for a contract, ordered by creation date descending.
-
-        Parameters:
-
-        contractId: Optional. Which contract to read comments for. Either an ID or slug must be specified.
-        contractSlug: Optional.
-        Requires no authorization.
-
-
-    GET /v0/bets
-    Gets a list of bets, ordered by creation date descending.
-
-    Parameters:
-
-    userId: Optional. If set, the response will include only bets created by this user.
-    username: Optional. If set, the response will include only bets created by this user.
-    contractId: Optional. If set, the response will only include bets on this contract.
-    contractSlug: Optional. If set, the response will only include bets on this contract.
-    limit: Optional. How many bets to return. The maximum and the default are 1000.
-    before: Optional. The ID of the bet before which the list will start. For example, if you ask for the most recent 10 bets, and then perform a second query for 10 more bets with before=[the id of the 10th bet], you will get bets 11 through 20.
-    Requires no authorization.
-                     */
-
     /// GET /v0/users
     /// Gets a list of users, ordered by creation date descending.
     /// Parameters:
@@ -408,6 +275,227 @@ impl ManifoldClient {
         }
 
         let response = self.http_post("/bet").json(&body).send().await?;
+        Ok(response.json().await?)
+    }
+
+    /// POST /v0/bet/[betId]/cancel
+    /// Cancel the limit order of a bet with the specified id. If the bet was unfilled, it will be cancelled so that no other bets will match with it. This action is irreversible.
+    pub async fn post_bet_cancel(&self, bet_id: &str) -> Result<Value> {
+        let response = self
+            .http_post(&format!("/bet/{}/cancel", bet_id))
+            .send()
+            .await?;
+        Ok(response.json().await?)
+    }
+
+    /// POST /v0/market
+    /// Creates a new market on behalf of the authorized user.
+    /// Note: this costs mana.
+    /// Parameters:
+    /// outcomeType: Required. One of BINARY, FREE_RESPONSE, MULTIPLE_CHOICE, or PSEUDO_NUMERIC.
+    /// question: Required. The headline question for the market.
+    /// description: Optional. A long description describing the rules for the market.
+    /// Note: string descriptions do not turn into links, mentions, formatted text. You may instead use descriptionMarkdown or descriptionHtml for rich text formatting.
+    /// closeTime: Optional. The time at which the market will close, represented as milliseconds since the epoch. Defaults to 7 days from now.
+    /// visibility: Optional. One of public (default) or unlisted. Controls whether the market can be shown on homepage and in search results.
+    /// groupId: Optional. A group to create this market under.
+    /// For binary markets, you must also provide:
+    ///
+    /// initialProb: An initial probability for the market, between 1 and 99.
+    /// For numeric markets, you must also provide:
+    ///
+    /// min: The minimum value that the market may resolve to.
+    /// max: The maximum value that the market may resolve to.
+    /// isLogScale: If true, your numeric market will increase exponentially from min to max.
+    /// initialValue: An initial value for the market, between min and max, exclusive.
+    /// For multiple choice markets, you must also provide:
+    ///
+    /// answers: An array of strings, each of which will be a valid answer for the market.
+    pub async fn post_market(&self, value: &Value) -> Result<FullMarket> {
+        let response = self.http_post("/market").json(&value).send().await?;
+        Ok(response.json().await?)
+    }
+
+    /// POST /v0/market/[marketId]/add-liquidity
+    /// Adds a specified amount of liquidity into the market.
+    /// amount: Required. The amount of liquidity to add, in M$.
+    pub async fn post_market_add_liquidity(&self, market_id: &str, amount: u64) -> Result<Value> {
+        let response = self
+            .http_post(&format!("/market/{}/add-liquidity", market_id))
+            .json(&json!({ "amount": amount }))
+            .send()
+            .await?;
+        Ok(response.json().await?)
+    }
+
+    /// POST /v0/market/[marketId]/close
+    /// Closes a market on behalf of the authorized user.
+    /// closeTime: Optional. Milliseconds since the epoch to close the market at. If not provided, the market will be closed immediately. Cannot provide close time in the past.
+    pub async fn post_market_close(
+        &self,
+        market_id: &str,
+        close_time: Option<u64>,
+    ) -> Result<Value> {
+        let mut body = json!({});
+        if let Some(close_time) = close_time {
+            body.as_object_mut()
+                .unwrap()
+                .insert("closeTime".to_owned(), json!(close_time.to_string()));
+        }
+
+        let response = self
+            .http_post(&format!("/market/{}/close", market_id))
+            .json(&body)
+            .send()
+            .await?;
+        Ok(response.json().await?)
+    }
+
+    /// POST /v0/market/[marketId]/resolve
+    /// Resolves a market on behalf of the authorized user.
+    /// Parameters:
+    ///
+    /// For binary markets:
+    ///
+    /// outcome: Required. One of YES, NO, MKT, or CANCEL.
+    /// probabilityInt: Optional. The probability to use for MKT resolution.
+    /// For free response or multiple choice markets:
+    ///
+    /// outcome: Required. One of MKT, CANCEL, or a number indicating the answer index.
+    /// resolutions: An array of { answer, pct } objects to use as the weights for resolving in favor of multiple free response /// options. Can only be set with MKT outcome. Note that the total weights must add to 100.
+    /// For numeric markets:
+    ///
+    /// outcome: Required. One of CANCEL, or a number indicating the selected numeric bucket ID.
+    /// value: The value that the market resolves to.
+    /// probabilityInt: Required if value is present. Should be equal to
+    /// If log scale: log10(value - min + 1) / log10(max - min + 1)
+    /// Otherwise: (value - min) / (max - min)
+    pub async fn post_market_resolve(&self, market_id: &str, value: &Value) -> Result<Value> {
+        let response = self
+            .http_post(&format!("/market/{}/resolve", market_id))
+            .json(&value)
+            .send()
+            .await?;
+        Ok(response.json().await?)
+    }
+
+    /// POST /v0/market/[marketId]/sell
+    /// Sells some quantity of shares in a binary market on behalf of the authorized user.
+    ///
+    /// Parameters:
+    ///
+    /// outcome: Optional. One of YES, or NO. If you leave it off, and you only own one kind of shares, you will sell that kind of shares.
+    /// shares: Optional. The amount of shares to sell of the outcome given above. If not provided, all the shares you own will be sold.
+    pub async fn post_market_sell(
+        &self,
+        market_id: &str,
+        outcome: Option<Outcome>,
+        shares: Option<u64>,
+    ) -> Result<Value> {
+        let mut body = json!({});
+
+        if let Some(outcome) = outcome {
+            body.as_object_mut()
+                .unwrap()
+                .insert("outcome".to_owned(), json!(outcome));
+        }
+
+        if let Some(shares) = shares {
+            body.as_object_mut()
+                .unwrap()
+                .insert("shares".to_owned(), json!(shares));
+        }
+
+        let response = self
+            .http_post(&format!("/market/{}/sell", market_id))
+            .json(&body)
+            .send()
+            .await?;
+        Ok(response.json().await?)
+    }
+
+    /// POST /v0/comment
+    /// Creates a comment in the specified market. Only supports top-level comments for now.
+    /// Parameters:
+    /// contractId: Required. The ID of the market to comment on.
+    /// content: The comment to post, formatted as TipTap json, OR
+    /// html: The comment to post, formatted as an HTML string, OR
+    /// markdown: The comment to post, formatted as a markdown string.
+    pub async fn post_comment(&self, value: &Value) -> Result<Value> {
+        let response = self.http_post("/comment").json(&value).send().await?;
+        Ok(response.json().await?)
+    }
+
+    /// GET /v0/comments
+    /// Gets a list of comments for a contract, ordered by creation date descending.
+    /// Parameters:
+    /// contractId: Optional. Which contract to read comments for. Either an ID or slug must be specified.
+    /// contractSlug: Optional.
+    /// Requires no authorization.
+    pub async fn get_comments(
+        &self,
+        contract_id: Option<&str>,
+        contract_slug: Option<&str>,
+    ) -> Result<Value> {
+        let mut url = "/comments".to_owned();
+        if let Some(contract_id) = contract_id {
+            url.push_str(&format!("?contractId={}", contract_id));
+        } else if let Some(contract_slug) = contract_slug {
+            url.push_str(&format!("?contractSlug={}", contract_slug));
+        }
+
+        let response = self.http_get(&url).send().await?;
+        Ok(response.json().await?)
+    }
+
+    /// GET /v0/bets
+    /// Gets a list of bets, ordered by creation date descending.
+    /// Parameters:
+    /// userId: Optional. If set, the response will include only bets created by this user.
+    /// username: Optional. If set, the response will include only bets created by this user.
+    /// contractId: Optional. If set, the response will only include bets on this contract.    
+    /// contractSlug: Optional. If set, the response will only include bets on this contract.
+    /// limit: Optional. How many bets to return. The maximum and the default are 1000.
+    /// before: Optional. The ID of the bet before which the list will start. For example, if you ask for the most recent 10 bets, and then perform a second query for 10 more bets with before=[the id of the 10th bet], you will get bets 11 through 20.
+    /// Requires no authorization.
+    pub async fn get_bets(
+        &self,
+        user_id: Option<&str>,
+        username: Option<&str>,
+        contract_id: Option<&str>,
+        contract_slug: Option<&str>,
+        limit: Option<u64>,
+        before: Option<&str>,
+    ) -> Result<Vec<Bet>> {
+        let mut query: Vec<(&str, &str)> = vec![];
+
+        if let Some(user_id) = user_id {
+            query.push(("userId", user_id));
+        }
+
+        if let Some(username) = username {
+            query.push(("username", username));
+        }
+
+        if let Some(contract_id) = contract_id {
+            query.push(("contractId", contract_id));
+        }
+
+        if let Some(contract_slug) = contract_slug {
+            query.push(("contractSlug", contract_slug));
+        }
+
+        let limit_s = limit.map(|l| l.to_string());
+
+        if let Some(limit) = limit_s.as_deref() {
+            query.push(("limit", limit));
+        }
+
+        if let Some(before) = before {
+            query.push(("before", before));
+        }
+
+        let response = self.http_get("/bets").query(&query).send().await?;
         Ok(response.json().await?)
     }
 }
